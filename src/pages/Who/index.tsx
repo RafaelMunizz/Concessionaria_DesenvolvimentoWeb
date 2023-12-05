6be1ce8b565397a89e76740d557ac7d4a7423069
Rafael Muniz <rafaelvictormuniz@gmail.com>
2023-12-05 00:05:23 -0300

    Quem somos

---
+++ src/pages/Who/index.tsx
@@ -0,0 +1,23 @@
+import { ImageTeam } from './styles';
+
+export function Who() {
+  return (
+    <div>
+      <br />
+          <h2>Quem Somos</h2>
+          <p>
+            Bem-vindo à nossa concessionária de carros! Somos uma equipe apaixonada por automóveis e comprometida em fornecer as melhores opções de veículos para nossos clientes.
+          </p>
+          <p>
+            Nossa missão é proporcionar uma experiência excepcional de compra de carros, garantindo a satisfação e confiança de nossos clientes.
+          </p>
+          <br />
+          <h3>Nossa Equipe</h3>
+          <p>
+            Conheça nossa equipe dedicada de profissionais que está aqui para ajudar você a encontrar o carro perfeito.
+          </p>
+          <ImageTeam src="src\assets\images\Equipe3.jpg" alt="Equipe"  />
+    </div>
+        
+  );
+}
---
+++ src/pages/Who/styles.ts
@@ -0,0 +1,6 @@
+import { Image } from "react-bootstrap";
+import styled from "styled-components";
+
+export const ImageTeam = styled.img`
+  width: 100%;
+`
