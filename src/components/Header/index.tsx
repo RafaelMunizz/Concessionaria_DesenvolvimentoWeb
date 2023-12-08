040a51e3deb2d7367dc3bae6c081639952daf398
Rafael Muniz <rafaelvictormuniz@gmail.com>
2023-12-08 00:26:31 -0300

    Mudança do nome da concessionária

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,16 +4,16 @@ export function Header() {
   return (
     <DivContainer>
       <DivTitle>
-        <h1>Concessionária Qualquer coisa</h1>
+        <h1>Concessionária </h1>
       </DivTitle>
       <DivImgCarros>
         <ImgCarros src="https://dicas.olx.com.br/wp-content/uploads/2021/04/tipos-de-carros-capa.png" alt="" />
       </DivImgCarros>
       <DivMenuSelection>
-      <ButtonsMenuSelection to={"/home"} >HOME</ButtonsMenuSelection>
-      <ButtonsMenuSelection to={"/posts"} >ANÚNCIOS</ButtonsMenuSelection>
-      <ButtonsMenuSelection to={"/who"} >QUEM SOMOS</ButtonsMenuSelection>
-      <ButtonsMenuSelection to={"/contact"} >CONTATE-NOS</ButtonsMenuSelection>
+        <ButtonsMenuSelection to={"/home"} >HOME</ButtonsMenuSelection>
+        <ButtonsMenuSelection to={"/posts"} >ANÚNCIOS</ButtonsMenuSelection>
+        <ButtonsMenuSelection to={"/who"} >QUEM SOMOS</ButtonsMenuSelection>
+        <ButtonsMenuSelection to={"/contact"} >CONTATE-NOS</ButtonsMenuSelection>
       </DivMenuSelection>
     </DivContainer>
   )
