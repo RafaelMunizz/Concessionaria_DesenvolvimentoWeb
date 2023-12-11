2d625262a9e64d23e4b39501c37905588e0a4fef
Rafael Muniz <rafaelvictormuniz@gmail.com>
2023-12-10 22:59:14 -0300

    Adição de página de contatos

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,7 +1,17 @@
+import { ButtonContact, DivConteinerContact, FormContact, InputFormContact, LabelFormContact, TitleContact } from "./styles";
+
 export function Contact() {
   return(
-    <div>
-      <h1>Contact</h1>
-    </div>
+    <DivConteinerContact>
+      <h1>Entre em contato conosco </h1>
+      <TitleContact>Digite seu email ou telefone que entraremos em contato </TitleContact>
+      <FormContact action="">
+        <LabelFormContact>Email</LabelFormContact>
+        <InputFormContact type="email" required/>
+        <LabelFormContact>Telefone</LabelFormContact>
+        <InputFormContact type="tel" required/>
+        <ButtonContact type="submit">Enviar</ButtonContact>
+      </FormContact>
+    </DivConteinerContact>
   )
 }
---
+++ src/pages/Contact/styles.ts
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const DivConteinerContact = styled.div`
+  padding: 5rem;
+`
+
+export const TitleContact = styled.h4`
+  margin: 2rem 0;
+`
+
+
+
+export const FormContact =   styled.form`
+display: flex;
+flex-direction: column;
+margin: 2rem 0;
+width: 20rem;
+`
+
+export const LabelFormContact = styled.label`
+  font-size: 2rem;
+`
+
+export const InputFormContact = styled.input`
+  border: 1px solid ${(props) => props.theme['gray-border']};
+  border-radius: 0.7rem;
+  margin: 0.5rem 0;
+  padding: 0.4rem;
+
+`
+
+export const ButtonContact = styled.button`
+  border: 0.2px solid black;
+  border-radius: 3rem;
+  background-color: #3636ff;
+  color: ${props => props.theme.white};
+  padding: 0.4rem;
+  margin-top: 1rem;
+`
